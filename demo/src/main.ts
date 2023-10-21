a1dc95def5a20729659664b1ffdd3bf39079295f
kobayashi <823913645@qq.com>
2023-10-21 21:17:01 +0800

    chore: add demo

--- demo/src/main.ts
+++ demo/src/main.ts
@@ -1,18 +1,20 @@
-// import MonacoThemeConverter from 'monaco-theme-converter'
-// import * as monaco from 'monaco-editor/esm/vs/editor/editor.api'
+import * as monaco from 'monaco-editor/esm/vs/editor/editor.api'
+import MonacoThemeConverter from 'monaco-theme-converter'
 
-// const converter = new MonacoThemeConverter({
-//   domain: 'danzzzz.netlify.app',
-//   path: '/resources',
-// })
+const converter = new MonacoThemeConverter({
+  domain: 'danzzzz.netlify.app',
+  path: '/resources',
+})
 
-// const editor = monaco.editor.create(document.getElementById('app')!, {
-//   language: 'javascript',
-//   value: 'class Test {}',
-//   automaticLayout: true,
-//   minimap: {
-//     enabled: false,
-//   },
-// })
+const editor = monaco.editor.create(document.getElementById('app')!, {
+  language: 'javascript',
+  value: 'class Test {}',
+  automaticLayout: true,
+  minimap: {
+    enabled: false,
+  },
+})
 
-// converter.setTheme(editor)
+converter.setTheme(editor, {
+  theme: 'VSDark',
+})
